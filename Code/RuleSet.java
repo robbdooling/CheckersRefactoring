1a7817df76ecef62cc7977eae0de03cb4eb47570
tjm7733 <tjm7733@rit.edu>
2013-11-27 14:18:38 -0500

    Add TODO to RuleSet
    Some ideas/concerns/work added to RuleSet, concerning its behavior.
    This is low priority, so this probably won't be addressed until later.

--- Code/RuleSet.java
+++ Code/RuleSet.java
@@ -10,7 +10,8 @@
 public abstract class RuleSet {
     
     // TODO Make Singleton?
-    
+    // TODO Look at inheriting classes for common methods; maybe create template
+    //      method with them?
     abstract boolean validateMove(Move move, Board board);
     abstract boolean canJump(Piece piece, Board board);
 }
