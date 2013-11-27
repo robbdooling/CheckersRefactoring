2f5fd546baae3502c0016acacc2090683fb92f67
tjm7733 <tjm7733@rit.edu>
2013-11-27 14:15:41 -0500

    Fill out SingleRuleSet
    SingleRuleSet now has complete logic for validating moves for Single-
    type pieces and can determine if one can make a jump.

--- Code/SingleRuleSet.java
+++ Code/SingleRuleSet.java
@@ -13,8 +13,22 @@ public class SingleRuleSet extends RuleSet {
      */
     @Override
     boolean validateMove(Move move, Board board) {
-        // TODO Auto-generated method stub
-        return false;
+
+	if (board.occupied(move.endLocation())) return false;
+
+	int moveDistance = move.endLocation() - move.getPiece.getLocation();
+	boolean flag = false;
+	for (int i : moveLocations(move.getPiece().getColor()) {
+	    if (moveDistance == i)
+                flag = true;
+	}
+        if (!flag) return false;
+	
+	if (moveDistance % 8 == 2 ||
+            moveDistance & 8 == 6 )
+            return canJump(move.getPiece(), board);
+	else
+	  return true;
     }
 
     /* (non-Javadoc)
@@ -22,8 +36,35 @@ public class SingleRuleSet extends RuleSet {
      */
     @Override
     boolean canJump(Piece piece, Board board) {
-        // TODO Auto-generated method stub
+	int[] locations = moveLocations(piece);
+
+	for (int i = 0; i < 2; i++) {
+	    if (locations[i] != Integer.MIN_VALUE)
+		continue;
+	    if (locations[i+2] == Integer.MIN_VALUE)
+		continue;
+	    if (board.colorAt(piece.getLocation() + (location[i+2]/2)) != piece.getColor())
+		return true;
+	
         return false;
+    }
+
+    private int[] moveLocations(Piece piece) {
+        int[] locations;
+        if (piece.getColor() == Color.blue)
+          locations = new int[] { -9,-7,-18,-14 };
+	else if (piece.getColor() == Color.white)
+          locations = new int[] { 7,9,14,18 };
+	else
+	  return null;
+
+	int pieceLocationMod = piece.getLocation() % 8;
+	if (pieceLocationMod == 0) locations[0] = Integer.MIN_VALUE;
+	if (pieceLocationMod == 7) locations[1] = Integer.MIN_VALUE;
+	if (pieceLocationMod == 1) locations[2] = Integer.MIN_VALUE;
+	if (pieceLocaitonMod == 6) locations[3] = Integer.MIN_VALUE;
+
+	return locations;
     }
 
 }
