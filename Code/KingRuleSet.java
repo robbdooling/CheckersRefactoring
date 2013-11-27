97524a8826d02d533791a34deded58798ae18ac0
tjm7733 <tjm7733@rit.edu>
2013-11-27 15:23:03 -0500

    Fix RuleSet incorrect functionality
    It has been determined that part of the functionality that the
    SingleRuleSet and KingRuleSet classes have implemented is incorrect,
    and needs to be fixed.

--- Code/KingRuleSet.java
+++ Code/KingRuleSet.java
@@ -25,5 +25,23 @@ public class KingRuleSet extends RuleSet {
         // TODO Auto-generated method stub
         return false;
     }
+    
+    private int[] moveLocations(Piece piece) {
+	int locations = new int[] { -9,-7,7,9,-18,-14,14,18 };
 
+	// Check vertical moves
+	int pieceLocationVerMod = (piece.getLocation() / 8) % 8;
+	if (pieceLocationVerMod < 2) {
+		for (int i = 0; i < 4 + (2 * pieceLocationVerMod); i += 4) {
+			locations[i]   = Integer.MIN_VALUE;
+			locations[i+1] = Integer.MIN_VALUE;
+		}
+	}
+
+	else if (pieceLocationVerMod > 5) {
+		for (int i = 2; i < 4 + (4 * (pieceLocationVerMod - 6); i += 4) {
+			locations[i] = Integer.MIN_VALUE;
+			locations[i+1] = Integer.MIN_VALUE;
+		}
+	}
 }
