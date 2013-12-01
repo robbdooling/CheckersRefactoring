17939dcfa42a9310cb33d2f9c1de1e1220e0c057
Ty <tjm7733@rit.edu>
2013-12-01 15:52:31 -0500

    Refactor RuleSet; Fix SingleRuleSet and KingRuleSet
    RuleSet has been changed to hold another method necessary for its child
    implementations; likewise, its original methods have now been defined 
    as a template for its own behavior. In addition, its children should 
    now have correct implementations of the moveLocations functions.

--- Code/RuleSet.java
+++ Code/RuleSet.java
@@ -3,15 +3,54 @@
  */
 
 /**
- * Interface for pieces to check moves for themselves
+ * Abstract class / inteface for pieces to check moves for themselves
  * @author Ty
  *
  */
 public abstract class RuleSet {
+    /** Check to see if the given move is valid. */
+    protected boolean validateMove(Move move, Board board) {
+        if (board.occupied(move.endLocation()))
+            return false;
+
+        int moveDistance = move.endLocation() - move.getPiece().getLocation();
+        boolean flag = false;
+        for (int i : moveLocations(move.getPiece())) {
+            if (moveDistance == i)
+                flag = true;
+        }
+        if (!flag)
+            return false;
+
+        if (moveDistance % 8 == 2 || moveDistance % 8 == 6)
+            return canJump(move.getPiece(), board);
+        else
+            return true;
+    }
     
-    // TODO Make Singleton?
-    // TODO Look at inheriting classes for common methods; maybe create template
-    //      method with them?
-    abstract boolean validateMove(Move move, Board board);
-    abstract boolean canJump(Piece piece, Board board);
+    /** Check to see if this piece can make a valid jump. */
+    protected boolean canJump(Piece piece, Board board) {
+        int[] locations = moveLocations(piece);
+        for (int i = 0; i < 2; i++) {
+            if (locations[i] != Integer.MIN_VALUE)
+                continue;
+            if (locations[i + 2] == Integer.MIN_VALUE)
+                continue;
+            if (board.colorAt(piece.getLocation() + (locations[i + 2] / 2)) != piece
+                    .getColor())
+                return true;
+        }
+        return false;
+    }
+    
+    /** 
+     * Generate all valid moves for a given piece.
+     * 
+     * @param piece - the piece object that moves are being generated for.
+     * 
+     * @return integer array of all moves; {@link Integer#MIN_VALUE} is used for
+     * invalid moves, while all other values are relative to the piece's 
+     * position in the board.
+     */
+    abstract protected int[] moveLocations(Piece piece);
 }
--- Code/SingleRuleSet.java
+++ Code/SingleRuleSet.java
@@ -1,83 +1,58 @@
 /**
  * SingleRuleSet.java
  */
+// TODO: Check to see that the validation logic is correct.
+
+import java.awt.Color;
 
 /**
  * @author Ty
- *
+ * 
  */
 public class SingleRuleSet extends RuleSet {
 
-    /* (non-Javadoc)
-     * @see RuleSet#validateMove(Move, Board)
+    /**
+     * Get all valid moves for a given piece.
+     * 
+     * Valid moves are determined by the Piece's color and its stored location.
+     * 
+     * @param piece - the piece object that moves are being generated for.
+     * @return integer array of all moves; {@link Integer#MIN_VALUE} is used for
+     * invalid moves, while all other values are relative to the piece's 
+     * position in the board.
      */
-    @Override
-    boolean validateMove(Move move, Board board) {
-
-	if (board.occupied(move.endLocation())) return false;
-
-	int moveDistance = move.endLocation() - move.getPiece.getLocation();
-	boolean flag = false;
-	for (int i : moveLocations(move.getPiece().getColor()) {
-	    if (moveDistance == i)
-                flag = true;
-	}
-        if (!flag) return false;
-	
-	if (moveDistance % 8 == 2 ||
-            moveDistance & 8 == 6 )
-            return canJump(move.getPiece(), board);
-	else
-	  return true;
-    }
-
-    /* (non-Javadoc)
-     * @see RuleSet#canJump(Piece, Board)
-     */
-    @Override
-    boolean canJump(Piece piece, Board board) {
-	int[] locations = moveLocations(piece);
-
-	for (int i = 0; i < 2; i++) {
-	    if (locations[i] != Integer.MIN_VALUE)
-		continue;
-	    if (locations[i+2] == Integer.MIN_VALUE)
-		continue;
-	    if (board.colorAt(piece.getLocation() + (location[i+2]/2)) != piece.getColor())
-		return true;
-	
-        return false;
-    }
-
-    private int[] moveLocations(Piece piece) {
+    protected int[] moveLocations(Piece piece) {
         int[] locations;
         if (piece.getColor() == Color.blue)
-          locations = new int[] { -9,-7,-18,-14 };
-	else if (piece.getColor() == Color.white)
-          locations = new int[] { 7,9,14,18 };
-	else
-	  return null;
+            locations = new int[] { -9, -7, -18, -14 };
+        else if (piece.getColor() == Color.white)
+            locations = new int[] { 7, 9, 14, 18 };
+        else
+            return null;
 
-	int pieceLocationVerMod = (piece.getLocation() / 8) % 8;
-	if (pieceLocationVerMod < 2 && locations[0] < 0) {
-		for (int i = 4 - (pieceLocationVerMod * 2); i < 4; i++) {
-			locations[i] = Integer.MIN_VALUE;
-		}
-	}
+        // Check vertical move validity
+        int pieceLocationVerMod = (piece.getLocation() / 8);
+        if (piece.getColor() == Color.blue && pieceLocationVerMod < 2) {
+            for (int i = pieceLocationVerMod * 2; i < 4; i++) {
+                locations[i] = Integer.MIN_VALUE;
+            }
+        } else if (piece.getColor() == Color.white && pieceLocationVerMod > 5) {
+            for (int i = (7 - pieceLocationVerMod) * 2; i < 4; i++) {
+                locations[i] = Integer.MIN_VALUE;
+            }
+        }
 
-	if (pieceLocationVerMod > 5 && locations[0] > 0) {
-		for (int i = 4 - ((pieceLocationVerMod - 6) * 2); i < 4; i++) {
-			locations[i[ = Integer.MIN_VALUE;
-		}
-	}
-	
-	int pieceLocationHorMod = piece.getLocation() % 8;
-	if (pieceLocationHorMod == 0) locations[0] = Integer.MIN_VALUE;
-	if (pieceLocationHorMod == 7) locations[1] = Integer.MIN_VALUE;
-	if (pieceLocationHorMod == 1) locations[2] = Integer.MIN_VALUE;
-	if (pieceLocationHorMod == 6) locations[3] = Integer.MIN_VALUE;
+        // Check horizontal move validity
+        int pieceLocationHorMod = piece.getLocation() % 8;
+        if (pieceLocationHorMod == 0)
+            locations[0] = Integer.MIN_VALUE;
+        if (pieceLocationHorMod == 7)
+            locations[1] = Integer.MIN_VALUE;
+        if (pieceLocationHorMod == 1)
+            locations[2] = Integer.MIN_VALUE;
+        if (pieceLocationHorMod == 6)
+            locations[3] = Integer.MIN_VALUE;
 
-	return locations;
+        return locations;
     }
-
 }
